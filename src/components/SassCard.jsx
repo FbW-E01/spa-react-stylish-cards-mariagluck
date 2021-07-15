dd52462eae6e1dd5f9c069f2da6f55dde0f567fc
mariagluck <mariamart.berlin@gmail.com>
2021-07-15 09:13:46 +0200

    added in comments code style to look identical

--- src/components/SassCard.jsx
+++ src/components/SassCard.jsx
@@ -19,3 +19,32 @@ function SassCard() {
 }
 
 export default SassCard;
+
+
+
+//Style to make the cards look identical:
+
+// import "./main.scss";
+// import whitePeak from "../images/jordan-steranka-lpddCskeg4A-unsplash.jpg";
+
+// function SassCard() {
+//   return (
+//     <div>
+//       <div className="sass_card">
+//         <div className="card_img">
+//           <img src={whitePeak} />
+//         </div>
+//         <div className="title_card">
+//           <h5>SASS Styled Card</h5>
+//           <p>
+//             This card has been styled using some cool SASS saved on separate
+//             file called main.
+//           </p>
+//           <button className="sassBtn">SASS</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default SassCard;
